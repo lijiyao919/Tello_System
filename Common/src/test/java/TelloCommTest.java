e08d0b58b5364bb95ab6e1945ea3d36fdf502d71
Jiyao Li <jiyao.li@aggiemail.usu.edu>
2019-09-25 19:23:30 -0600

    TelloCommTest (figure out port bind)

--- Common/src/test/java/TelloCommTest.java
+++ Common/src/test/java/TelloCommTest.java
@@ -5,20 +5,20 @@ import java.net.InetAddress;
 import java.util.Arrays;
 
 public class TelloCommTest {
-    //@Test
-    //public void testSendAndReceiveMsg() throws Exception {
-    public static void main(String args[]) throws Exception{
+    @Test
+    public void testSendAndReceiveMsg() throws Exception {
+    //public static void main(String args[]) throws Exception{
         TelloComm dc1 = new TelloComm(InetAddress.getByName("127.0.0.1"), 0);
+        TelloComm dc2 = new TelloComm(InetAddress.getByName("127.0.0.1"), 8890);
         byte[] cmdSend = new byte[64];
         byte[] cmdRec;
 
         cmdSend[0] = 99;
         cmdSend[1] = 111;
         cmdSend[2] = 109;
-        dc1.sendMsg(cmdSend, "127.0.0.1", 8889);
+        dc1.sendMsg(cmdSend, "127.0.0.1", 8890);
         Thread.sleep(1000);
 
-        TelloComm dc2 = new TelloComm(InetAddress.getByName("127.0.0.1"), 8890);
         System.out.println(Arrays.toString(cmdSend));
         cmdRec = dc2.receiveMsg();
         System.out.println(Arrays.toString(cmdRec));
