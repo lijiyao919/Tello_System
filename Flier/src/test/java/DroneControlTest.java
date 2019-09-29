e27e05891e1ce1dfdc3d89b6aaa7e6dee9912bc9
Jiyao Li <jiyao.li@aggiemail.usu.edu>
2019-09-28 21:13:06 -0600

    fix dronecontroltest

--- Flier/src/test/java/DroneControlTest.java
+++ Flier/src/test/java/DroneControlTest.java
@@ -5,7 +5,8 @@ import org.junit.Test;
 public class DroneControlTest {
     @Test
     public void testDoMissions() throws Exception{
-        DroneControl dc = new DroneControl();
+        DroneState ds = new DroneState();
+        DroneControl dc = new DroneControl(ds);
         int[] MissionChoice = {1,2,3};
         Boolean result = dc.doMissions(3,MissionChoice);
         Assert.assertEquals(Boolean.TRUE, result);
